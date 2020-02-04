4c999153ec40063094907ec5b057de27b3a4d551
Praxis <tommyopeters@gmail.com>
2020-02-04 13:39:51 +0100

    CSS for the logo and chance/community-chest place holder

--- src/components/Center.js
+++ src/components/Center.js
@@ -5,12 +5,22 @@ const Center = () => {
     <div className="center">
       <div className="community-chest-deck">
         <h2 className="label">Community Chest</h2>
-        <div className="deck"></div>
+        <div className="deck">
+        <div class="card-box card-blue">
+						<div class="card-blue-inside"></div>
+					</div>
+        </div>
       </div>
-      <h1 className="title">MONOPOLY</h1>
+      <div className="title-box">
+        <span className="title">MONOPOLY</span>
+      </div>
       <div className="chance-deck">
         <h2 className="label">Chance</h2>
-        <div className="deck"></div>
+        <div className="deck">
+        <div class="card-box card-orange">
+						<div class="card-orange-inside"></div>
+					</div>
+        </div>
       </div>
     </div>
   );
