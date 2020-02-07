d77c0b1c4cadc92887dbca0cb7b3b58e40c85b11
Tommy O' Peters <tommyopeters>
2020-02-07 07:21:09 +0100

    Move to work PC

---
+++ src/assets/plain-board.js
@@ -0,0 +1,9 @@
+let leftRow = document.querySelector(".left-row");
+let leftRowContainers = document.querySelectorAll(".left-row .space");
+
+// console.log(leftRow.offsetHeight);
+
+// leftRowContainers.forEach(el => {
+//   el.style.width = leftRow.offsetHeight / 9 + "px";
+//   console.log(leftRow.offsetHeight / 9);
+// });
