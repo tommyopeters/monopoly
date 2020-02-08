3b1a2270036a9e78c4a9315f3192ce2019aecd0b
Tommy O' Peters <tommyopeters>
2020-02-08 04:42:32 +0100

    Working inside Assets folder to non-destructively create the correct css/scss code

--- src/assets/plain-board.js
+++ src/assets/plain-board.js
@@ -1,9 +1,25 @@
-let leftRow = document.querySelector(".left-row");
-let leftRowContainers = document.querySelectorAll(".left-row .space");
+// let leftRow = document.querySelector(".left-row");
+let leftRowSpaces = document.querySelectorAll(".left-row .space");
+let leftRowContainers = document.querySelectorAll(
+  ".left-row .space .container"
+);
 
-// console.log(leftRow.offsetHeight);
+leftRowContainers.forEach(el => {
+  el.style.width = leftRowSpaces[0].offsetHeight + "px";
+  el.style.height = leftRowSpaces[0].offsetWidth + "px";
 
-// leftRowContainers.forEach(el => {
-//   el.style.width = leftRow.offsetHeight / 9 + "px";
-//   console.log(leftRow.offsetHeight / 9);
-// });
+  console.log(el.offsetHeight);
+});
+
+// let rightRow = document.querySelector(".right-row");
+let rightRowSpaces = document.querySelectorAll(".right-row .space");
+let rightRowContainers = document.querySelectorAll(
+  ".right-row .space .container"
+);
+
+rightRowContainers.forEach(el => {
+  el.style.width = rightRowSpaces[0].offsetHeight + "px";
+  el.style.height = rightRowSpaces[0].offsetWidth + "px";
+
+  console.log(el.offsetHeight);
+});
